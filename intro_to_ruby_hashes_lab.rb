dda0f9642631aacfe662f7ee36fb95cd51f9c614
Will Hodgson <will.hodgson@current-rms.com>
2019-07-30 10:22:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -58,9 +58,11 @@ def monopoly_with_fourth_tier
           :mortgage_value => "200"
         },
         :b_and_o_railroad => {
-          :mortgage_value = "$400"
+          :mortgage_value => "$400"
         },
-        :shortline => {}
+        :shortline => {
+          :mortgage_value => "$500"
+        }
       },
       :rent_in_dollars => {
         :one_piece_owned => 25,
